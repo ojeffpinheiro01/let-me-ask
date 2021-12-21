208c449f8cebc2f4e46b9c59267122dc26c35127
ojeffpinheiro01 <jefersonpinheirodesouza@gmail.com>
2021-12-21 20:44:46 -0300

    aula01

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/Button";
+
+function App() {
+  return (
+    <>
+      <h1>HELOOOO</h1>
+      <Button  />
+      <Button  />
+    </>
+  );
+}
+
+export default App;
